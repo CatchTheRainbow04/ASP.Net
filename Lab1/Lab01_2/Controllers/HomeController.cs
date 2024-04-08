834c56ed1fc2b5f81f1ba0c5ba7705dfff53e5ee
CatchTheRainbow04 <doanhdoe@gmail.com>
2024-04-08 19:55:02 +0700

    hehe

---
+++ Lab1/Lab01_2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Research IT - DEVMASTER / Chúng tôi sẽ làm bạn hài lòng!";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Thông tin liên hệ";
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            ViewBag.Message = "Thực đơn hôm nay";
+            string[] products = {"Gà quay","Cá chép om dưa","Vịt quay vân đình","Ốc xào xả ớt"};
+            ViewBag.products = products;
+            return View();
+        }
+    }
+}
---
+++ Lab1/Lab1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.message = "Chào mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+    }
+}
