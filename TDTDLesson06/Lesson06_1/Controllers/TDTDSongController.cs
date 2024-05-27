8ba3294a1e3b236a0e368fe970997c8f524132d1
CatchTheRainbow04 <doanhdoe@gmail.com>
2024-05-27 19:23:06 +0700

    lesson6

---
+++ TDTDLesson06/Lesson06_1/Controllers/TDTDSongController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lesson06_1.Models;
+
+namespace Lesson06_1.Controllers
+{
+    public class TDTDSongController : Controller
+    {
+        public static List<TDTDSong> TDTDSongs = new List<TDTDSong>()
+        {
+            new TDTDSong{ ID = 1 ,TDTDTitle = "Jotaro Theme", TDTDAuthor = "Jotaro" , TDTDArtist = "Kujo Jotaro", TDTDYearRelease = 2020},
+            new TDTDSong{ ID = 2 ,TDTDTitle = "Giorno Theme", TDTDAuthor = "Giorno",TDTDArtist = "Giorno Giovanna", TDTDYearRelease = 2020},
+        };
+        // GET: TDTDSong
+        public ActionResult TDTDIndex()
+        {
+            return View(TDTDSongs);
+        }
+        public ActionResult TDTDCreate()
+        {
+            var TDTDsong = new TDTDSong();
+            return View(TDTDsong);
+        }
+        [HttpPost]
+        public ActionResult TDTDCreate(TDTDSong tDTDSong)
+        {
+            if(!ModelState.IsValid) // lỗi
+            {
+                return View(tDTDSong);
+            }
+            TDTDSongs.Add(tDTDSong);
+            return RedirectToAction("TDTDIndex");
+        }
+    }
+}
---
+++ TDTDLesson06/Lesson06_1/Models/TDTDSong.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lesson06_1.Models
+{
+    public class TDTDSong
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Enter Song Name !")]
+        [DisplayName("Tiêu đề")]
+        public string TDTDTitle { get; set; }
+
+        [Required(ErrorMessage ="Enter Author Name !")]
+        [DisplayName("Nghệ sĩ")]
+        public string TDTDAuthor { get; set; }
+
+        [Required(ErrorMessage ="Enter Artist Name !")]
+        [StringLength(100,MinimumLength = 2,ErrorMessage = "The artist's name must be more than 2 characters and less than 100 characters !")]
+        [DisplayName("Tác giả")]
+
+        public string TDTDArtist { get; set; }
+
+        [Required(ErrorMessage ="Enter Year Release")]
+        [RegularExpression(@"[0-9]{4}",ErrorMessage = "The year release must include 4 digits !")]
+        [Range(1900,2024,ErrorMessage = "The year release must be in the years [1900-2024]")]
+        [DisplayName("Năm ra mắt")]
+        public int TDTDYearRelease { get; set; }
+    }
+}
